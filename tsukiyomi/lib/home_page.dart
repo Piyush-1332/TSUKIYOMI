2602380726b330744d2ac912aa344d034ffa116a
pankajchamyal <pankajchamyal@gmail.com>
2019-10-06 11:39:23 +0530

    first commit

---
+++ tsukiyomi/lib/home_page.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: [Color(0x990000), Colors.black],
+              begin: Alignment.topCenter,
+              end: FractionalOffset(0.3, 0.3))),
+      child: ListView(
+        children: <Widget>[
+          SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.all(20.0),
+                  height: 20.0,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      Icon(FontAwesomeIcons.gamepad),
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(20.0, 45.0, 0.0, 0.0),
+                  height: 100.0,
+                  child: Column(
+                    children: <Widget>[
+                      Text(
+                        'Recently Played',
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(1.0),
+                          fontSize: 23.0,fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 160.0,
+                  child: ListView.builder(
+                    itemCount: 10,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Column(
+                        children: <Widget>[
+                          SizedBox(
+                            height: 120.0,
+                            width: 145.0,
+                            child: Image.network(
+                              'https://www.mobygames.com/images/covers/l/55382-super-mario-advance-4-super-mario-bros-3-game-boy-advance-front-cover.jpg',
+                              fit: BoxFit.fitHeight,
+                            ),
+                          )
+                        ],
+                      );
+                    },
+                  ),
+                ),
+                /*comment*/
+                Container(
+                  padding: EdgeInsets.fromLTRB(20.0,20.0, 0.0, 0.0),
+                  height: 70.0,
+                  child: Column(
+                    children: <Widget>[
+                      Text(
+                        'Made For You',
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(1.0),
+                          fontSize: 23.0,fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                /*comment*/
+                Container(
+                  height: 270.0,
+                  child: ListView.builder(
+                    itemCount: 10,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Column(
+                        children: <Widget>[
+                          SizedBox(
+                            height: 230.0,
+                            width: 280.0,
+                            child: Image.network(
+                              'https://www.mobygames.com/images/covers/l/55382-super-mario-advance-4-super-mario-bros-3-game-boy-advance-front-cover.jpg',
+                              fit: BoxFit.fitHeight,
+                            ),
+                          )
+                        ],
+                      );
+                    },
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(20.0, 20.0, 0.0, 0.0),
+                  height: 80.0,
+                  child: Column(
+                    children: <Widget>[
+                      Text(
+                        'Popular & Trending',
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(1.0),
+                          fontSize: 23.0,fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                /*comment*/
+                Container(
+                  height: 200.0,
+                  child: ListView.builder(
+                    itemCount: 10,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Column(
+                        children: <Widget>[
+                          SizedBox(
+                            height: 170.0,
+                            width: 210.0,
+                            child: Image.network(
+                              'https://www.mobygames.com/images/covers/l/55382-super-mario-advance-4-super-mario-bros-3-game-boy-advance-front-cover.jpg',
+                              fit: BoxFit.fitHeight,
+                            ),
+                          )
+                        ],
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ tsukiyomi/lib/start_page.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:tsukiyomi/home_page.dart';
+import 'package:tsukiyomi/browse_page.dart';
+import 'package:tsukiyomi/library_page.dart';
+import 'package:tsukiyomi/more_page.dart';
+class Start extends StatefulWidget {
+  @override
+  _StartState createState() => _StartState();
+}
+
+class _StartState extends State<Start> {
+  int index=0;
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: show(index),
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(
+          canvasColor: Color(0xAA000000),
+        ),
+        child: BottomNavigationBar(
+          currentIndex: index,
+          
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.redAccent,
+          unselectedFontSize: 9,
+          selectedFontSize: 11,
+          onTap: ((int x){
+            setState(() {
+             index = x;
+             show(index);
+            });
+          }),
+          
+          items: [
+            new BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              title: Text('Home'),
+              
+            ),
+            new BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              title: Text('Browse'), 
+            ),
+            new BottomNavigationBarItem(
+              icon: Icon(Icons.file_download),
+              title: Text('Your Library'),
+              
+            ),
+            new BottomNavigationBarItem(
+              icon: Icon(Icons.list),
+              title: Text('More'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+Widget show(int index){
+  switch(index){
+    case 0: return HomePage();
+    break;
+    case 1: return Browse();
+    break;
+    case 2: return Library();
+    break;
+    case 3: return More();
+    break;
+    default: return HomePage();
+    break;
+  }
+}
